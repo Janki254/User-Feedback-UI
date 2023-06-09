925b1d046ea3a8dba1481b2231f55cef1546ad11
Janki <janki.gohil@odaninfotech.com>
2023-06-09 22:56:24 +0530

    Add code for User-Feedback-UI.

---
+++ feedback.js
@@ -0,0 +1,41 @@
+const ratings = document.querySelectorAll('.rating');
+const ratingContainer = document.querySelector('.rating-container');
+const sendBtn = document.querySelector('#send');
+const panel = document.querySelector('#panel');
+
+let selectedRating = `Satisfied`;
+
+ratingContainer.addEventListener('click', (e) => {
+    if (
+        e.target.parentNode.classList.contains('rating') &&
+        e.target.nextElementSibling
+    ) {
+        removeActive();
+        e.target.parentNode.classList.add('active');
+        selectedRating = e.nextElementSibling.innerHTML;
+    } else if (
+        e.target.parentNode.classList.contains('rating') &&
+        e.target.previousSibling &&
+        e.target.previousElementSibling.nodeName === 'IMG'
+    ) {
+        removeActive();
+        e.target.parentNode.classList.add('active');
+        selectedRating = e.target.innerHTML;
+    }
+});
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>Thank You!.</strong>
+        <br/>
+        <strong class="feedback-text">Feedback : ${selectedRating}</strong>
+        <p class="feedback-text">We will use your feedback to improve our customer support</p>
+    `;
+});
+
+function removeActive() {
+    for (let i = 0; i < ratings.length; i++) {
+        ratings[i].classList.remove('active');
+    }
+}
